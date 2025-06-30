5ab4974b90e1e54be0450645ed2dc2fc6f32e265
Maciel Barbosa <macielbarbosa@outlook.com>
2025-06-30 04:49:59 -0300

    Total dos itens do Carrinho

---
+++ src/pages/CartPage/components/Resume/index.tsx
@@ -0,0 +1,38 @@
+import { useMemo } from "react";
+import { useAtomValue } from "jotai";
+import { Flex, Heading, Text } from "@radix-ui/themes";
+
+import { useCartState } from "@/state/cart";
+import { productsAtom } from "@/state/products";
+import { toCurrency } from "@/utils/toCurrency";
+
+export const Resume = () => {
+  const { cart } = useCartState();
+  const products = useAtomValue(productsAtom);
+
+  const getPrice = (id: string) => {
+    const product = products.find((product) => product.id === id);
+    return product!.price;
+  };
+
+  const total = useMemo(
+    () =>
+      products.length
+        ? cart.reduce(
+            (total, item) => total + getPrice(item.id) * item.quantity,
+            0
+          )
+        : 0,
+    [cart, products]
+  );
+
+  return (
+    <Flex direction="column">
+      <Heading size="4" mb="3">
+        Resumo
+      </Heading>
+      <Text>Valor dos produtos:</Text>
+      <Text weight="medium">{toCurrency(total)}</Text>
+    </Flex>
+  );
+};
--- src/pages/CartPage/style.ts
+++ src/pages/CartPage/style.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Root = styled.div`
   display: flex;
   justify-content: space-between;
+  gap: 2rem;
+  flex-wrap: wrap;
 `;
 
 export const Items = styled.div`
